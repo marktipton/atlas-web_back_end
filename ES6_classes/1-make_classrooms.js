c06da048ca19ec7a182c36909d2c71288909133a
mrktptn <marktipton95@gmail.com>
2024-01-04 11:24:35 -0600

    added new keyword when creating instances of ClassRoom

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const class1 = ClassRoom(19);
-  const class2 = ClassRoom(20);
-  const class3 = ClassRoom(34);
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
   const classArray = [class1, class2, class3];
   return classArray;
 }

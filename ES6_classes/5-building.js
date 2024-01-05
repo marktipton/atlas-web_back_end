0eae1af7974b511327c714c38334dbad17832452
mrktptn <marktipton95@gmail.com>
2024-01-05 12:14:47 -0600

    try not checking building class type, just checking evacuationerrormessage method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Abstract class Building cannot be instantiated directly');
-    }
+    // if (new.target === Building) {
+    //   throw new TypeError('Abstract class Building cannot be instantiated directly');
+    // }
     this._sqft = sqft;
     if (typeof this.evacuationWarningMessage !== 'function') {
       throw new TypeError('Class extending Building must override evacuationWarningMessage.');

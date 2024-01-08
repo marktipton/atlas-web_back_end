365b37d01068a3472240c1e2cac4fad1acbec898
mrktptn <marktipton95@gmail.com>
2024-01-08 11:20:26 -0600

    lint fix

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(studentsList) {
+  // add id to the accumulator w/ initial value set to zero below
   const idSum = studentsList.reduce((accumulator, student) => {
-    // add id to the accumulator w/ initial value set to zero below
     return accumulator + student.id;
   }, 0);
   return Number(idSum);

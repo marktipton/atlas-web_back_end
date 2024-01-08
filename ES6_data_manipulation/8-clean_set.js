119122db562445c4ce24a386b1825bf53668e8c8
mrktptn <marktipton95@gmail.com>
2024-01-08 16:36:24 -0600

    adding check for type string and casting output as string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
   const cleanString = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length));
-  return cleanString.join('-');
+  return String(cleanString.join('-'));
 }

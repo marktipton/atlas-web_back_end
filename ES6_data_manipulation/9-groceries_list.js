d25487196ab751bbe126e11d27b8e332e798bf96
mrktptn <marktipton95@gmail.com>
2024-01-08 14:23:35 -0600

    trying to make map from grocery list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  groceryList = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  }
+  groceryMap = Map(groceriesList);
+  return groceryMap;
+}

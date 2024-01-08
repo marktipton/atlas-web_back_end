1542b208a95187437beb38f0294705c0a7637d79
mrktptn <marktipton95@gmail.com>
2024-01-08 14:04:34 -0600

    trying to return nothing

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   if (startString === '') {
-    return '';
+    return;
   }
   const cleanString = Array.from(set)
-    .filter(value => value.startsWith(startString))
-    .map(value => value.substring(startString.length));
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length));
   return cleanString.join('-');
 }

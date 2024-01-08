b792340cd8d41acd4cd0cf6a8e9c3947e8b354f0
mrktptn <marktipton95@gmail.com>
2024-01-08 11:24:20 -0600

    comma dangle:

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 export default function getStudentIdsSum(studentsList) {
   // add id to the accumulator w/ initial value set to zero below
   const idSum = studentsList.reduce(
-    (accumulator, student) => accumulator + student.id, 0
+    (accumulator, student) => accumulator + student.id, 0,
   );
   return Number(idSum);
 }

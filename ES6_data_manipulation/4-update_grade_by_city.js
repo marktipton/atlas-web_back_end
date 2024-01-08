37e173d1931e07c833be53d368b3ace741901172
mrktptn <marktipton95@gmail.com>
2024-01-08 12:29:29 -0600

    lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
-  const studentsByLocation = studentsList.filter((students) => students.location === city)
-  return studentsByLocation.map((student) => { const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+  const studentsByLocation = studentsList.filter((students) => students.location === city);
+  return studentsByLocation.map((student) =>
+  { const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
     if (gradeObj) {
       return { ...student, grade: gradeObj.grade };
-    } else {
-      return student;
     }
+      return student;
   });
 }

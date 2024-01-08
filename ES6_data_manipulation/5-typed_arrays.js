ec25cdd53f81af09be9aef0f4af9af8a54a5043c
mrktptn <marktipton95@gmail.com>
2024-01-08 16:47:31 -0600

    added dataview representation for buffer

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,7 +5,9 @@ export default function createInt8TypedArray(length, position, value) {
 
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
+
   int8Array[position] = value;
 
-  return buffer;
+  const dataView = new DataView(buffer);
+  return dataView;
 }

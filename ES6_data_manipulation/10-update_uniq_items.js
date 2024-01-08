396c22861727e2783dba0b869766a683458cc6c6
mrktptn <marktipton95@gmail.com>
2024-01-08 14:57:52 -0600

    using forEach and .set to set new

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqueItems(foodMap) {
+  if (!(foodMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  const updatedMap = new Map();
+
+  foodMap.forEach((quantity, item) => {
+    if (quantity === 1) {
+      //replace with 100
+      updatedMap.set(item, 100);
+    } else {
+      //use original value
+      updatedMap.set(item, quantity);
+    }
+  })
+}

56c701f373fc473e31b7f7fe810afb594054a6e5
mrktptn <marktipton95@gmail.com>
2024-01-08 14:01:19 -0600

    return empty string if start string is empty

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   const cleanString = Array.from(set)
     .filter(value => value.startsWith(startString))
     .map(value => value.substring(startString.length));

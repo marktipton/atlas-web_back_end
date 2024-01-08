924d923c56fb0f4d911fa5b61491f0297e4a27bb
mrktptn <marktipton95@gmail.com>
2024-01-08 10:08:30 -0600

    return

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -4,4 +4,5 @@ export default function getListStudents() {
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
+  return objectArray;
 }

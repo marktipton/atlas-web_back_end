511ab4e544e7ba651cf6edd04ef0f2b37d1d9dc9
mrktptn <marktipton95@gmail.com>
2024-01-09 19:13:26 -0600

    lint fix

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
 export default function getResponseFromAPI() {
   const myPromise = new Promise((resolve, reject) => {
-    let a = 1 + 1;
-    if (a == 2) {
+    const a = 1 + 1;
+    if (a === 2) {
       resolve('Success');
     } else {
-      reject('Failed');
+      reject(new Error('Failed'));
     }
-  })
+  });
 
   return myPromise;
 }

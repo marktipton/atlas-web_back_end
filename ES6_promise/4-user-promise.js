f192921a03bc9e2a8cd216012d4eb7d3ab52c07a
mrktptn <marktipton95@gmail.com>
2024-01-09 19:45:36 -0600

    return object when promise resolved

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,12 @@
+export default function signUpUser(firstName, lastName) {
+  const promise = new Promise((resolve, reject) => {
+    const a = true;
+    if (a) {
+      const promiseObject = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      resolve(promiseObject);
+    }
+  });
+}

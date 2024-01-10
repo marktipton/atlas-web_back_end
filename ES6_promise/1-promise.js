e9010670944f3dbe222a86c9f299c5569c014a93
mrktptn <marktipton95@gmail.com>
2024-01-09 19:21:06 -0600

    made promise with specific resolve and reject

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      const apiResponse = {
+        status: 200,
+        body: 'Success'
+      };
+      resolse(apiResponse);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}

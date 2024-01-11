c9ae0e502fcc6146759f946cf1b24ca0677b88cc
mrktptn <marktipton95@gmail.com>
2024-01-10 18:34:18 -0600

    return promise w/ resolve and reject for jpg file

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,9 @@
+export default function uploadPhoto(fileName) {
+  return new Promise((resolve, reject) => {
+    if (fileName.endsWith('.jpg')) {
+      resolve(`File ${fileName} uploaded successfully.`);
+    } else {
+      reject(new Error(`Error: ${fileName} cannot be processed.`));
+    }
+  });
+}

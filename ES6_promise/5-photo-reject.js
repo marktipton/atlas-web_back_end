0f1970631e78dece0a673d533c9526243df7da0f
mrktptn <marktipton95@gmail.com>
2024-01-10 18:38:25 -0600

    lint fix

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    const a = true
+    const a = true;
     if (!a) {
       resolve(`File ${fileName} uploaded successfully.`);
     } else {

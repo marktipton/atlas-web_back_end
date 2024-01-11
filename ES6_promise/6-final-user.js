6a8184cbf29c68490af3e06452cfa497c5a9fae1
mrktptn <marktipton95@gmail.com>
2024-01-10 20:13:36 -0600

    just return promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,11 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) =>
-      results.map((result) => ({
-        status: result.status === 'fulfilled' ? 'fulfilled' : 'rejected',
-        ...(result.status === 'fulfilled' ? { value: result.value } : { reason: result.reason.message }),
-      }))
-    );
+  return userPromise + photoPromise;
 }

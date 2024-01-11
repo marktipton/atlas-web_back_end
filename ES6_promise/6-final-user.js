6775f9f430d240413bd65da202dfe9914cbe54c5
mrktptn <marktipton95@gmail.com>
2024-01-10 20:03:36 -0600

    importing promises from user and phtp and using promise.allsettled to return an array

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      return results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return { status: 'fulfilled', value: result.value };
+        } else {
+          return { status: 'rejected', reason: result.reason.message };
+        }
+      });
+    });
+}

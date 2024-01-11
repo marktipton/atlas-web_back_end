4442a35ee81066b9a5e20da65e3dbbd649e6217d
mrktptn <marktipton95@gmail.com>
2024-01-10 19:14:25 -0600

    using promise.all method to resolve all promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,3 +1,13 @@
-export default function handleProfileSignup() {
+import {uploadPhoto, createUser} from './5-photo-reject';
 
+export default function handleProfileSignup() {
+  const promises = [uploadPhoto(), createUser()];
+
+  return Promise.all(promises)
+    .then(([photo, user]) => {
+      console.log(`Body: ${user.firtsName} ${user.lastName}`);
+    })
+    .catch((error) => {
+      console.error('Signup system offline', error);
+    });
 }

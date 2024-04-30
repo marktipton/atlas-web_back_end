b3ea583e615ed6e0d851f555bb995e757472c58c
mrktptn <marktipton95@gmail.com>
2024-04-30 15:11:01 -0500

    concatenate strings

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Type annoteated function for concatenating strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns concatenated strings as str"""
+    return str1 + str2

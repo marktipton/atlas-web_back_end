deaa28225ae0f3a4a767053dd74304b4fa6e2a7f
mrktptn <marktipton95@gmail.com>
2024-04-30 16:41:41 -0500

    if it looks like a duck then its probably a duck

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""duck type for an iterable object"""
+from typing import Iterable, List, Sequence, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> Tuple(Sequence, int):
+    """duck typing for an iterable object"""
+    return [(i, len(i)) for i in lst]

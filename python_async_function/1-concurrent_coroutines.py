9fe71b912460e7c5b0a61de7f064bae7738f72cc
mrktptn <marktipton95@gmail.com>
2024-04-30 17:46:08 -0500

    added import for List

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Use wait_random function and make function to spawn n wait_randoms"""
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """creates n wait_random functions with specified max_delay"""
     wait_times = []
 

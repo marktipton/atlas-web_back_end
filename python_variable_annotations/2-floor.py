da9bfa79b4870f8c56c708a07bc02b47ebbda0b7
mrktptn <marktipton95@gmail.com>
2024-04-30 15:22:39 -0500

    int(n)

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """type annotated function to return floor of float"""
-import math
 
 
 def floor(n: float) -> int:
     """convert floor to int to get floor"""
-    return n
+    return int(n)

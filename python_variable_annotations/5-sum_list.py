9a8dcf422419b2f8997e3df70b8497f8a47770c1
mrktptn <marktipton95@gmail.com>
2024-04-30 15:46:12 -0500

    assign and type annotate sum variable

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """returns sum of list of floats"""
+    sum: float
     for floats in input_list:
         sum += floats
     return sum

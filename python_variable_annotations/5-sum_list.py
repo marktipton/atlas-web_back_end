4d8d2592d36b2a86ede3a8bf9baf6f3d6f192798
mrktptn <marktipton95@gmail.com>
2024-04-30 15:48:40 -0500

    change variable name

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,7 +5,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """returns sum of list of floats"""
-    sum: float
+    total: float
     for floats in input_list:
-        sum += floats
-    return sum
+        total += floats
+    return total

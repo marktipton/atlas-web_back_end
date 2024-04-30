8f5b875b718ceb8b0c8dba5912f371e7b4470616
mrktptn <marktipton95@gmail.com>
2024-04-30 15:55:14 -0500

    mixed type list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""mixed type list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """returns float sum of mixed type list"""
+    return sum(mxd_lst)
+

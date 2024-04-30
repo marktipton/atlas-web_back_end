e24f54c81aaf01dcbc00d4ec121121cbf36df894
mrktptn <marktipton95@gmail.com>
2024-04-30 15:40:59 -0500

    return sum

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""return sum of list of floats"""
+
+
+def sum_list(input_list) -> float:
+    """"""
+    for floats in input_list:
+        sum += floats
+    return sum
--- python_variable_annotations/README.md
+++ python_variable_annotations/README.md
@@ -1,1 +1,3 @@
 # Type Annotations in Python
+
+- If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck.

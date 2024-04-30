d8295f42f58d12f09eeda38cd71854cf1c6b974e
mrktptn <marktipton95@gmail.com>
2024-04-30 16:00:01 -0500

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""complex types converting string and int/float to tuple"""
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """takes string and number and returns tuple with both"""
+    return tuple(k, v)

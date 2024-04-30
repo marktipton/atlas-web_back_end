5d0b18620144e9d1211d3cd0db436ec14c41653c
mrktptn <marktipton95@gmail.com>
2024-04-30 15:42:27 -0500

    added annotation for input list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,7 +2,7 @@
 """return sum of list of floats"""
 
 
-def sum_list(input_list) -> float:
+def sum_list(input_list: float) -> float:
     """returns sum of list of floats"""
     for floats in input_list:
         sum += floats

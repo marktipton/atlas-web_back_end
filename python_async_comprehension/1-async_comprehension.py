a4e27661c71646f3771abd5b93785728ba7b1718
marktipton <6675@holbertonstudents.com>
2024-05-02 20:11:40 +0000

    added comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""adding async comprehension coroutine"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async comprehension that collects numbers generated"""
+    # THIS IS A COMPREHENSION!!!!!!!!!!!!!!!!!!!
+    numbers = [number async for number in async_generator()]
+    return numbers

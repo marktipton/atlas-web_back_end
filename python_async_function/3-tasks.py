a3203aaad7e9ca517c70f1a8a80a00dd74974afe
marktipton <6675@holbertonstudents.com>
2024-05-02 16:02:34 +0000

    push from docker

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Using regular (non-async) function """
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """returns the asyncio.Task of the wait_random function"""
+    wait_random(max_delay)

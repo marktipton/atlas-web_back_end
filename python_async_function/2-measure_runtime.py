8e1512369987cc7bea26c98791798eedf8e4295e
mrktptn <marktipton95@gmail.com>
2024-05-02 10:30:53 -0500

    added asyncio.run before calling wait function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -11,7 +11,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """returns the average time for n wait_n functions to run"""
     start = time.time()
-    wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
     total_time = end - start
     return (total_time / n)

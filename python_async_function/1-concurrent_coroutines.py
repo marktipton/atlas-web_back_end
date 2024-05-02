3955bdbd715402b90c9cdb8358d82153182eddc0
mrktptn <marktipton95@gmail.com>
2024-05-02 10:23:41 -0500

    added function to measure time of wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,8 +11,10 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """creates n wait_random functions with specified max_delay"""
     wait_times = []
 
+    # create list of coroutine tasks
     tasks = [wait_random(max_delay) for i in range(n)]
 
+    # Await completion of tasks as completed
     for task in asyncio.as_completed(tasks):
         wait_time = await task
         wait_times.append(wait_time)
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Measures runtime of wait_n function"""
+import asyncio
+import time
+from typing import List
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """returns the average time for n wait_n functions to run"""
+    start = time.time()
+    wait_n(n, max_delay)
+    end = time.time()
+    total_time = end - start
+    return (total_time / n)

bbe7d2d0eaadeb92c69a992289a4b90aa1ccf1d0
marktipton <6675@holbertonstudents.com>
2024-05-02 18:46:01 +0000

    added random number list and return list

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""coroutine that creates 10 random numbers between 1 and 10"""
+
+import asyncio
+import random
+from typing import List
+
+async def async_generator() -> List[int]:
+    random_number_list = []
+    i = 0
+    while (i < 10):
+        i += 1
+        asyncio.sleep(1)
+        random_number_list += random.uniform(0, 10)
+
+    return random_number_list

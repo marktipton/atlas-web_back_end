deb12456256fff966dbe9037c18ab46c435e1791
marktipton <6675@holbertonstudents.com>
2024-05-02 16:54:38 +0000

    importing task_wait_random instead of wait_random

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""Changing wait_n into task_wait_n"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """creates n wait_random functions with specified max_delay"""
+    wait_times = []
+
+    # create list of coroutine tasks
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+
+    # Await completion of tasks as completed
+    for task in asyncio.as_completed(tasks):
+        wait_time = await task
+        wait_times.append(wait_time)
+
+    return wait_times

d924d39f3bb7f7ed8a3b8068595cc5d0068b6ce2
marktipton <6675@holbertonstudents.com>
2024-05-02 16:48:45 +0000

    asyncio.Task

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,4 +8,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """returns the asyncio.Task of the wait_random function"""
-    wait_random(max_delay)
+    return asyncio.Task(wait_random(max_delay))

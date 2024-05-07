55b12086e10d4cc15c8c8cecc6ff72fd9135024c
marktipton <6675@holbertonstudents.com>
2024-05-07 19:09:19 +0000

    added logic to put and get methods

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -8,11 +8,20 @@ class BasicCache(BaseCaching):
     def __init__(self, key, item):
         """initialize child class"""
         super().__init__()
+        self.cache_data(key) = item
 
     def put(self, key, item):
         """add cache data w/ key value pair"""
-        self.cache_data(key) = item
+        if (key or item is None) {
+            return
+        } else {
+            self.cache_data(key) = item
+        }
 
     def get(self, key):
         """retrieve cache data using key value pair"""
-        for key in sorted(self.cache_data.keys()):
+        if (key is None or key not in self.cache_data) {
+            return None
+        } else {
+            return self.cache_data(key)
+        }

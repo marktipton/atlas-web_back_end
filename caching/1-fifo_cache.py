a9031b9f9e2d30b107500567ee468f47b0a8fa6d
marktipton <6675@holbertonstudents.com>
2024-05-08 19:16:42 +0000

    added variable to track insertion order for FIFO

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""Creates FIFO caching system"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """First in first out caching"""
+    def __init__(self):
+        """overload constructor"""
+        super().__init__()
+        self.insertion_order = []
+
+    def put(self, key, item):
+        """assigns item to key in dictionary if not none"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+
+
+
+
+    def get(self, key):
+        """gets the value in self.cache_data linked to a specified key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)

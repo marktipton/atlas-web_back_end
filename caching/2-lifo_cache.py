60247d25a9b5a0e1c240836bd6694e3150945969
marktipton <6675@holbertonstudents.com>
2024-05-08 20:00:23 +0000

    added import to files

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,1 +1,4 @@
 #!/usr/bin/env python3
+"""Last in first out cache"""
+BaseCaching = __import__('base_caching').BaseCaching
+
--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python3
+"""Last recently used cache"""
+BaseCaching = __import__('base_caching').BaseCaching

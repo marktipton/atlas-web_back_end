adbac8d2aa38d01f9e9fffa8ab9e6691f7b33278
marktipton <6675@holbertonstudents.com>
2024-05-09 16:48:00 +0000

    added another dictionary to track insertion order

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -5,14 +5,21 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class LIFOCache(BaseCaching):
     """Last in first out caching"""
+    def __init__(self):
+        """overload constructor"""
+        super().__init__()
+        self.insertion_order = []
 
     def put(self, key, item):
         """assigns item to key in dictionary if not none"""
         if key is None or item is None:
             return
         self.cache_data[key] = item
+        self.insertion_order.append(key)
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = list(self.cache_data.keys())[-1]
+            # get last key and pop that item from the stack
+            last_key = self.insertion_order.pop()
             self.cache_data.pop(last_key)
             print(f'DISCARD: {last_key}')
 

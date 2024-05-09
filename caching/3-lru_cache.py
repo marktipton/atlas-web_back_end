13538b5a92e45d200c9fa6b6b4d563b4bf7dc35e
marktipton <6675@holbertonstudents.com>
2024-05-09 18:23:42 +0000

    made outline of LRU

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """Last recently used cache"""
 BaseCaching = __import__('base_caching').BaseCaching
+
+class LRUCache(BaseCaching):
+    """Last recently used cache"""
+
+    def put(self, key, item):
+        """assigns item to key in dictionary if not none"""
+
+    def get(self, key):
+        """gets the value in self.cache_data linked to a specified key"""

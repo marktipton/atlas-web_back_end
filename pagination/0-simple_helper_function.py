e35ebd894d2fc0cfa15f4e4403c17ec88e811ce0
marktipton <6675@holbertonstudents.com>
2024-05-12 17:24:52 +0000

    returning start index and end index

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Helper function to get tuple of size"""
+
+
+def index_range(page, page_size):
+    """return start index and end index for pagination"""
+    start_index = (page - 1) * page_size
+    return (start_index, page * page_size)

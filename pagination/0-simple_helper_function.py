df084d3124cabb5bb68946f7f775dc38822f2f2f
marktipton <6675@holbertonstudents.com>
2024-05-12 17:25:47 +0000

    added variable for clarity

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,4 +5,5 @@
 def index_range(page, page_size):
     """return start index and end index for pagination"""
     start_index = (page - 1) * page_size
-    return (start_index, page * page_size)
+    end_index = page * page_size
+    return (start_index, end_index)

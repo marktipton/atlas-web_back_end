477f1ab7024f72a5fd848ca2a76a3c071f02a28a
marktipton <6675@holbertonstudents.com>
2024-05-16 00:54:17 +0000

    getting typeerror on first attempt at format method

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -30,8 +30,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION,
+                        record, self.SEPARATOR)

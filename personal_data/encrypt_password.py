d1ceb7e358cf2539209ae17680f47c6342d3f502
mrktptn <marktipton95@gmail.com>
2024-05-19 17:12:50 -0500

    returning bytes not bytearray

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ Password ENcryption using hashing.
 import bcrypt
 
 
-def hash_password(password: str) -> bytearray:
+def hash_password(password: str) -> bytes:
     """returns hashed and salted password"""
     # convert pw to an array of bytes
     byteConversion = password.encode('utf-8')

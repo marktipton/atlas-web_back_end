fbc4ce055cca37dfef41823158887d8db4fae937
mrktptn <marktipton95@gmail.com>
2024-05-19 17:49:42 -0500

    check pw hash using bcrypt.checkpw

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -16,3 +16,12 @@ def hash_password(password: str) -> bytes:
     hashedPassword = bcrypt.hashpw(byteConversion, salt)
 
     return(hashedPassword)
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if password is valid, returns false if not"""
+    # convert user entered pw to bytes
+    userPwBytes = password.encode('utf-8')
+    # check user pw against hashed pw
+    result = bcrypt.checkpw(userPwBytes, hashed_password)
+
+    return(result)

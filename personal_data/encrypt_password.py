b2f2405f7b48098533d8c5cca08361e7b04e889a
mrktptn <marktipton95@gmail.com>
2024-05-19 17:11:08 -0500

    hashed pw using bcrypt package

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Password ENcryption using hashing.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytearray:
+    """returns hashed and salted password"""
+    # convert pw to an array of bytes
+    byteConversion = password.encode('utf-8')
+
+    # generate random data for hashing function
+    salt = bcrypt.gensalt()
+
+    hashedPassword = bcrypt.hashpw(byteConversion, salt)
+
+    return(hashedPassword)

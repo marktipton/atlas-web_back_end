f46787562a64ff089cb689411ceb1197857b6e8e
mrktptn <marktipton95@gmail.com>
2024-05-20 15:54:06 -0500

    normalized paths to achieve slash tolerance to check which paths need authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,18 @@ class Auth():
     """Class to manage API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False"""
-        return False
+        """Returns true if path does not need authentication else false"""
+        if (path is None or excluded_paths is None):
+            return True
+        if excluded_paths == []:
+            return True
+        # strip trailing slashes to make slash "tolerant"
+        ST_path = path.rstrip('/')
+        ST_excluded_paths = [p.rstrip('/') for p in excluded_paths]
+        if ST_path in ST_excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """returns None"""

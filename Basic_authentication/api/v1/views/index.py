ae9e316fc7d561f7ec7a4dd4b023fb10bf791287
mrktptn <marktipton95@gmail.com>
2024-05-20 13:11:11 -0500

    added error handler and new endpoint

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the status of the API
+    """
+    return jsonify({"error": "Unauthorized"})

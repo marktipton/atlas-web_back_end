58e036d5e69397d37707cad00a9cd98219a3b4e7
mrktptn <marktipton95@gmail.com>
2024-05-20 15:30:47 -0500

    added auth folder, class, and methods

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Managing API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Class to manage API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """returns None"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None"""
+        return None

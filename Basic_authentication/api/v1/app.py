70ca029958915e5e59566f69711ac5eb6718340f
mrktptn <marktipton95@gmail.com>
2024-05-20 16:38:05 -0500

    added outline of before request and authorization header function

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -6,13 +6,20 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = None
+auth = Auth()
+excluded_paths = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/'
+]
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -34,6 +41,16 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+def before_request():
+    """Filter requests"""
+    if auth == None:
+        pass
+    if auth.require_auth(request.path, excluded_paths) == False:
+        pass
+    if auth.authorization_header(request) == None:
+        abort(401)
+    if auth.current_user(request) == None:
+        abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,13 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """returns None"""
-        return None
+        Authorization = request.header.get("Authorization")
+        if request is None:
+            return None
+        if Authorization in request:
+            return Authorization
+        else:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None"""

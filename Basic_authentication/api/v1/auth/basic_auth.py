bf44ed6e67f15dc1f533c838bad315ec911023f4
mrktptn <marktipton95@gmail.com>
2024-05-21 14:29:02 -0500

    added base64n auth method to basicauth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,12 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """basic authentication"""
+    def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
+        """Gets the Base64 part of the auth and returns it"""
+        if authorization_header is None or \
+            not isinstance(authorization_header, str) or \
+               not authorization_header.startswith('Basic '):
+            return None
+        # otherwise return value after basic
+        return authorization_header.split(' ')[1]

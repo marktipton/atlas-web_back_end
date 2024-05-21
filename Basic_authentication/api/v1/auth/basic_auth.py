6df3f97977ea40b7420f0fc7e025e0f80957f4db
mrktptn <marktipton95@gmail.com>
2024-05-21 14:58:19 -0500

    decodebase64

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Managing API basic authentication"""
+import base64
 from flask import request
 from typing import List, TypeVar
 from .auth import Auth
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
             return None
         """otherwise return value after basic"""
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns decoded value of Base64 string"""
+        if base64_authorization_header is None or \
+            not isinstance(base64_authorization_header, str) or \
+                not base64_authorization_header:
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            decodedutf8 = decoded.decode('utf-8')
+            return decodedutf8
+        except Exception as e:
+            pass

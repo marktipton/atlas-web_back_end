79b405d23fae15cca54dc449c209196b06dd3e6a
mrktptn <marktipton95@gmail.com>
2024-05-21 14:38:48 -0500

    pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,11 +8,11 @@ from .auth import Auth
 class BasicAuth(Auth):
     """basic authentication"""
     def extract_base64_authorization_header(
-    self, authorization_header: str) -> str:
+            self, authorization_header: str) -> str:
         """Gets the Base64 part of the auth and returns it"""
         if authorization_header is None or \
             not isinstance(authorization_header, str) or \
                 not authorization_header.startswith('Basic '):
             return None
-        # otherwise return value after basic
+        """otherwise return value after basic"""
         return authorization_header.split(' ')[1]

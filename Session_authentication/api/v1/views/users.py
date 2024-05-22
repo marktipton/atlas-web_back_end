21e1b84541da686c61aeed1b4917d61361b02501
mrktptn <marktipton95@gmail.com>
2024-05-22 14:02:22 -0500

    updates to Get user method

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -25,9 +25,10 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id is None:
+    if user_id == 'me' and request.current_user is None:
         abort(404)
-    user = User.get(user_id)
+    if user_id == 'me' and request.current_user:
+        user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())

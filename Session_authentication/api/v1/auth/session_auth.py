df4391b5975ffcbafbddfde738bc2fe5e5ba2dde
mrktptn <marktipton95@gmail.com>
2024-05-23 11:05:52 -0500

    creating session id using UUID, adding session ID to user_id_by_session dictionary

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Managing API session authentication"""
 import base64
 import logging
+import uuid
 from flask import request
 from typing import List, TypeVar, Tuple
 from .auth import Auth
@@ -10,3 +11,12 @@ from models.user import User
 
 class SessionAuth(Auth):
     """Session Authentication class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates session ID for a user ID"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

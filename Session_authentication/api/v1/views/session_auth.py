ecd6c603f82cc24a9b2e3dc6848053e3dcc4f870
mrktptn <marktipton95@gmail.com>
2024-05-23 16:45:47 -0500

    updated post route

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -7,7 +7,10 @@ from models.user import User
 from os import getenv
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route(
+    '/api/v1/auth_session/login',
+    methods=['POST'],
+    strict_slashes=False)
 def status() -> str:
     """ POST /api/v1/auth_session/login
     Return:

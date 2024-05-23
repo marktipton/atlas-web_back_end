1e5c1353d98898c05acb79bbf2576c96a9bf939b
mrktptn <marktipton95@gmail.com>
2024-05-23 15:51:53 -0500

    user id FOR session not BY session FUNCTION, not dictionary

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -34,7 +34,7 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         if session_id is None:
             return None
-        user_id = self.user_id_by_session_id(session_id)
+        user_id = self.user_id_for_session_id(session_id)
         if user_id is None:
             return None
         return User.get(user_id)

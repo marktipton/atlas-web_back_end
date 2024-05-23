fa69418c5144c555a2df3a1f6255c79aeeb618e8
mrktptn <marktipton95@gmail.com>
2024-05-23 11:46:28 -0500

    edited before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -27,7 +27,8 @@ elif AUTH_TYPE == 'session_auth':
 excluded_paths = [
     '/api/v1/status/',
     '/api/v1/unauthorized/',
-    '/api/v1/forbidden/'
+    '/api/v1/forbidden/',
+    '/api/v1/auth_session/login/'
 ]
 
 
@@ -62,6 +63,9 @@ def before_request():
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)

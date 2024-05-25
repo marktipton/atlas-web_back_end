439f1787405c14d7082885b2862417fadd329d0e
mrktptn <marktipton95@gmail.com>
2024-05-25 00:24:32 -0500

    added character limit to strings

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -8,15 +8,16 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     """User database table sqlalchemy"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
 
     # def __repr__(self):
     #     return "<User(name='%s', fullname='%s', nickname='%s')>" % (

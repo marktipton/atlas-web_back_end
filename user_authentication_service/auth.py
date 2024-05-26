5b282421118d4d3596f3de926c24572ff8182ce8
mrktptn <marktipton95@gmail.com>
2024-05-26 15:05:26 -0500

    adding reset token for user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -81,3 +81,15 @@ class Auth:
         """destroys session by setting session id to None"""
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """fings user corresponding to email, generates
+        a UUID and updates the user's reset token
+        """
+        user = self._db.find_user_by(email=email)
+
+        if not user:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token

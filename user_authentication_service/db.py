dcf8d2b46c883c58b04285c8fcaabe844516b842
mrktptn <marktipton95@gmail.com>
2024-05-25 19:20:38 -0500

    added import and new method declaration

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound
 
 from user import Base, User
 
@@ -38,3 +39,6 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self):
+        """returns the first row in the users table"""

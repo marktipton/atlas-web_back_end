f2ce60213a5f6d8f5e3db832a74c9a2a0d9470ef
mrktptn <marktipton95@gmail.com>
2024-05-26 00:14:09 -0500

    json flask test

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 """
-App
+Flask App
 """
+from os import getenv
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+@app.route('', methods=['GET'])
+def jsonPayload():
+    """returns JSON payload"""
+    return {"message": "Bienvenue"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")

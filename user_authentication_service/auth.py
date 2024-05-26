d897e2566bab34fac4ab2a456ce1b261042d22b5
mrktptn <marktipton95@gmail.com>
2024-05-25 23:26:17 -0500

    added user to db with hashed pw

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 User Authentication
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,3 +18,20 @@ def _hash_password(password: str) -> bytes:
     hashedPassword = bcrypt.hashpw(byteConversion, salt)
 
     return(hashedPassword)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers user by hashing pw if the user doesnt already exist"""
+        if self._db.find_user_by(email):
+            ValueError(f"User {email} already exists")
+
+        hashed_pw = _hash_password(password)
+        registered_user = self._db.add_user(email, hashed_pw)
+        return registered_user

c49e067443c1a15b5872065b37f38bb0cbc626f8
mrktptn <marktipton95@gmail.com>
2024-05-26 14:42:02 -0500

    change inputs to update user method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -79,6 +79,5 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """destroys session by setting session id to None"""
-        user = self._db.find_user_by(id=user_id)
-        user.update_user(session_id=None)
+        self._db.update_user(user_id, session_id=None)
         return None

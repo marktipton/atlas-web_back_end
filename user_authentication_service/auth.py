8951fddd4ef0c9765ffab872d7efc4c7221b04f0
mrktptn <marktipton95@gmail.com>
2024-05-25 23:06:54 -0500

    move hash pw outside of class so it is function instead of method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,3 +2,17 @@
 """
 User Authentication
 """
+import bcrypt
+
+# class Auth:
+def _hash_password(password: str) -> bytes:
+    """takes pw and returns salted hash version"""
+    # convert pw to an array of bytes
+    byteConversion = password.encode('utf-8')
+
+    # generate random data for hashing function
+    salt = bcrypt.gensalt()
+
+    hashedPassword = bcrypt.hashpw(byteConversion, salt)
+
+    return(hashedPassword)

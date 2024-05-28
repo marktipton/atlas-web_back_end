11dbb4c211bf0954e289bf58927d1ddd0afd1a70
mrktptn <marktipton95@gmail.com>
2024-05-28 13:56:08 -0500

    added imports for test_client module and parameters

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,4 +2,20 @@
 """Unittests for client
 """
 import unittest
+from client import GithubOrgClient
+from parameterized import parameterized
+from unittest.mock import patch, Mock
+from utils import get_json
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests to test_org method from client"""
+    @parameterized.expand([
+        "google",
+        "abc"
+    ])
+    @patch('get_json')
+    def test_org(self, org):
+        """test test_org method from client"""
+        self.assertEqual(GithubOrgClient.org(org))
 

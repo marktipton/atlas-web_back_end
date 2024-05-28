c7429d136447cce12c04dd4785d6a76d27e2d85c
mrktptn <marktipton95@gmail.com>
2024-05-28 15:17:10 -0500

    added more patching to check the public_repos method

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -33,8 +33,12 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         """unit-test fpr GithubOrgClient _public_repos_url property"""
+
+        # known payload containig expected value for _public_repos_url
         test_payload = {"test_url": "http://example.com/test"}
 
+        # patch the org method of GithubOrgClient so that the mocked method is
+        # called instead of the actual method
         with patch.object(GithubOrgClient, 'org') as mock_org:
             mock_org.return_value = test_payload
 
@@ -44,6 +48,27 @@ class TestGithubOrgClient(unittest.TestCase):
 
             self.assertEqual(result, test_payload["test_url"])
 
+    @patch('client.get_json')
+    @patch(GithubOrgClient, '_public_repos_url')
+    def test_public_repos(self, mock_repo_url, mock_get_json):
+        """test for public_repos method in client"""
+        mock_payload = [{"name": "repo1"}, {"name": "repo2"}]
+
+        mock_repo_url.return_value = "http://example.com/test"
+
+        mock_get_json.return_value = mock_payload
+
+        client = GithubOrgClient("example_org")
+
+        repo = client.public_repos()
+        # check that result is the list of repos from the mock payload
+        self.assertEqual(repo, ["repo1", "repo2"])
+
+        # check that _public_repos_url was only called once
+        mock_repo_url.assert_called_once()
+
+        # check that get_json was called once with mocked url
+        mock_get_json.assert_called_once_with("http://example.com/test")
 
 if __name__ == '__main__':
     unittest.main()

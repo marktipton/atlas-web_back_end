ff088a77f469d7baf84ccf31e2f9468d8f36a8c9
mrktptn <marktipton95@gmail.com>
2024-05-30 10:24:26 -0500

    using different parameterize class

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from client import GithubOrgClient
-from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
+from fixtures import TEST_PAYLOAD
 from parameterized import parameterized, parameterized_class
 from unittest.mock import patch, Mock
 
@@ -86,9 +86,10 @@ class TestGithubOrgClient(unittest.TestCase):
 
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integreation test for public repos method"""
-    @parameterized_class(('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'), [
-        (org_payload, repos_payload, expected_repos, apache2_repos)
-    ])
+    @parameterized_class(
+        ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+        TEST_PAYLOAD
+    )
 
     @classmethod
     def setUpClass(cls) -> None:

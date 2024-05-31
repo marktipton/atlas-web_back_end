c1e342a4bc124ff50b65908399734503b0aea314
mrktptn <marktipton95@gmail.com>
2024-05-31 12:56:45 -0500

    added typing for data in store

--- 0x0B_redis_basic/README.md
+++ 0x0B_redis_basic/README.md
@@ -1,3 +1,16 @@
 # Redis Python
 
-- Redis is
+- Redis is an open source in-memory data structure store. It operates like a NoSQL Key/Value Store but supports multiple data structures and types including:
+
+  - strings
+  - lists
+  - sets
+  - sorted sets
+  - hashes
+  - bitmaps
+  - hyperlogs
+  - geospatial indexes
+
+- Redis can act as both a caching system and a database
+
+- Redis can be used with virtually every programming language
--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,3 +2,19 @@
 """
 Exercise operations Redis
 """
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """redis caching"""
+    def __init__(self) -> None:
+        # store instance of redis client
+        _redis = redis.Redis()
+        # flush instance
+        _redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generates a random key and stores"""
+        random_key = uuid.uuid4()

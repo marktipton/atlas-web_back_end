f8a7d32c77712acbee0dc642012c47551a105a07
mrktptn <marktipton95@gmail.com>
2024-06-01 11:23:20 -0500

    count call decorator now above store method. no longer getting errors but getting None output

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -11,13 +11,25 @@ from typing import Union, Callable, Optional, Any
 def count_calls(method: Callable) -> Callable:
     """decorator function to count calls of another function or class"""
     @wraps(method)
-    def wrapper(*args, **kwds):
+    def wrapper(self, *args, **kwds):
         """wrapper"""
-        print('Calling decorated function')
-        return method(*args, **kwds)
+        cls = self.__class__
+        # initialize _call_counts attribute if it does not exist in class
+        if not hasattr(cls, '_call_counts'):
+            # dictionary for storing call counts for each method
+            cls._call_counts = {}
+        # check if qualified name is not already a key
+        # in the _call_counts dictionary
+        if method.__qualname__ not in cls._call_counts:
+            # since the qualname is not in the dictioanry, init its count to 0
+            cls._call_counts[method.__qualname__] = 0
+        # otherwise, qualname is in the dictionary
+        # so increment the value to count call
+        cls._call_counts[method.__qualname__] += 1
+        return method(self, *args, **kwds)
     return wrapper
 
-@count_calls
+
 class Cache:
     """redis caching"""
     def __init__(self):
@@ -26,6 +38,7 @@ class Cache:
         # flush instance
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generates a random key and stores"""
         random_key = str(uuid.uuid4())

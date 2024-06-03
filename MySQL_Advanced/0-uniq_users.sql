4b0f806d8e9521cf929f509cea1ece622170d17d
mrktptn <marktipton95@gmail.com>
2024-06-03 12:16:58 -0500

    created user table

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- user table
+-- with id, email, and name
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+);

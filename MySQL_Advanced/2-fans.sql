e4ed9adc48589e068764bd39e5e130d54cc9d82e
mrktptn <marktipton95@gmail.com>
2024-06-03 13:22:28 -0500

    made ranking of fans by country of origin

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,2 +1,14 @@
 -- ranks country origins of bands by number of fans
 
+CREATE OR REPLACE VIEW fan_rank_by_country AS
+SELECT
+  origin,
+  SUM(fans) AS nb_fans
+FROM
+  metal_bands
+GROUP BY
+  origin
+ORDER BY
+  nb_fans DESC;
+
+SELECT * FROM fan_rank_by_country

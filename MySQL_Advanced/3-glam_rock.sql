a694c8d040f374d7402dcb18b1b5201e5e6a84e6
mrktptn <marktipton95@gmail.com>
2024-06-04 12:42:10 -0500

    use 2020 as year instead of current year

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE VIEW glam_rock_by_longevity AS
 SELECT
   band_name,
   -- if split is NONE in table then current date is used instead
-  COALESCE(split, YEAR(CURDATE())) - formed AS lifespan
+  COALESCE(split, YEAR(CURDATE()) - 4) - formed AS lifespan
 FROM
   metal_bands
 WHERE

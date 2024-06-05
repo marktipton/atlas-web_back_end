f251e507375ed76ed40bb3a78c05e2ef9b191ef2
mrktptn <marktipton95@gmail.com>
2024-06-05 11:06:37 -0500

    adding begin end and new delimiter

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,11 @@
 -- Trigger reset attributes when email is changed
-
+DELIMITER //
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-
+BEGIN
   IF NEW.email <> OLD.email THEN
-    SET NEW.valid_email = FALSE;
+  SET NEW.valid_email = FALSE;
   END IF;
-
+END //
+DELIMITER;

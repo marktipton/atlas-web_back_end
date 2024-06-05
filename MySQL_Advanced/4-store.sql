e023d02974e79d32ea13c02ef3301db944671ea6
mrktptn <marktipton95@gmail.com>
2024-06-05 10:59:41 -0500

    taking begin end off of 4-store

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -4,8 +4,6 @@
 CREATE TRIGGER decrease_item_when_ordered
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.number
-  WHERE name = NEW.item_name;
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;

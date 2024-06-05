808abe3ce46e70f871b5d0ec21c984a41760161e
mrktptn <marktipton95@gmail.com>
2024-06-04 23:18:28 -0500

    created need_meeting view but not getting any output

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,12 @@
+-- new view need_meeting that lists students with grade C and lower
+-- but only if these students haven't already had a meeting in the last month
+
+CREATE VIEW need_meeting AS
+SELECT
+  * FROM students
+  WHERE score < 80
+  AND last_meeting = NULL
+  OR last_meeting > 1;
+
+
+SELECT * FROM need_meeting;

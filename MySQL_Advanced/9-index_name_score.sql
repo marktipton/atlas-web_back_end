48b8d9b2827e3e4ffed2496f56281b14c6ee39ff
mrktptn <marktipton95@gmail.com>
2024-06-08 15:14:50 -0500

    add index for name in same file

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,7 @@
 -- creates index on the table names and the first letter of score
 
 -- create index without adding column
+
+CREATE INDEX idx_name_first ON names (name(1));
+
 CREATE INDEX idx_name_first_score ON names (score);

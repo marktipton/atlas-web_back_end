5f7ad7968a2d04c7e9248d8c5a41f329451cbc08
mrktptn <marktipton95@gmail.com>
2024-06-08 15:05:10 -0500

    creating index on first letter directly instead of adding column

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,10 +1,5 @@
 -- creates index on the table names and the first letter of name
 
--- Add column that contains the first letter of the name column
--- using LEFT(name, 1)
-ALTER TABLE names
-ADD COLUMN first_letter CHAR(1) generated ALWAYS AS (LEFT(name, 1)) STORED;
-
--- create index on generated column
-CREATE INDEX idx_name_first ON names (first_letter);
+-- create index without adding column
+CREATE INDEX idx_name_first ON names (name(1));
 

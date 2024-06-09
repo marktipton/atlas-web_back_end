30ac5afdc4b53369747256d1c524d3273b3da954
mrktptn <marktipton95@gmail.com>
2024-06-09 12:23:00 -0500

    only select name

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -3,7 +3,7 @@
 
 CREATE OR REPLACE VIEW need_meeting AS
 SELECT
-  name, score, last_meeting
+  name
 FROM students
 WHERE score < 80
 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));

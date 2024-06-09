153503e11066d620eeb8dae3a479728ae78e6616
mrktptn <marktipton95@gmail.com>
2024-06-09 12:20:31 -0500

    add another query to check last_meeting status

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,11 +1,12 @@
 -- new view need_meeting that lists students with grade C and lower
 -- but only if these students haven't already had a meeting in the last month
 
-CREATE VIEW need_meeting AS
+CREATE OR REPLACE VIEW need_meeting AS
 SELECT
-  name, score, last_meeting FROM students
-  WHERE score < 80
-  AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
-
-
+  name, score, last_meeting
+FROM students
+WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+-- query students table to check if meeting has already taken place
+SELECT name, score, ISNULL(last_meeting) FROM students;
 SELECT * FROM need_meeting;

d6eed1a61e07d48d860008c245ad3bd733476dc8
mrktptn <marktipton95@gmail.com>
2024-06-11 17:28:44 -0500

    added log for documents with filters for # of documents by method and path

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+nginx_collection = client.logs.nginx
+
+num_logs = nginx_collection.count_documents({})
+print(f"{num_logs} logs")
+
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("Methods:")
+for method in methods:
+    method_count = nginx_collection.count_documents({"method": method})
+    print(f"\tmethod {method}: {method_count}")
+
+status_count = nginx_collection.count_documents(
+    {"method": "GET", "path": "/status"}
+)
+print(f"{status_count} status check")

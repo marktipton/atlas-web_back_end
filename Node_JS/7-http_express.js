9ca5f5d658f030b6ebdf451ad1adc8b2a589472a
mrktptn <marktipton95@gmail.com>
2024-06-19 11:26:42 -0500

    students express node server working correctly

--- Node_JS/7-http_express.js
+++ Node_JS/7-http_express.js
@@ -11,26 +11,21 @@ app.get("/students", async function (request_obj, response_obj) {
   // const requestUrl = url.parse(request_obj.url, true);
   // const { pathname } = requestUrl;
 
-  // if (pathname === '/') {
-  //   response_obj.writeHead(200, { 'Content-Type': 'text/plain'});
-  //   response_obj.end('Hello Holberton School!');
-  // } else if (pathname === '/students') {
-  //   response_obj.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  //   const databasePath = process.argv[2];
-  //   if (!databasePath) {
-  //     response_obj.end('Database name must be provided as an argument');
-  //     return;
-  //   }
-  //   try {
-  //     // use await bc countstudents is
-  //     // an async operation which returns a promise
-  //     const students = await countStudents(databasePath);
-  //     response_obj.end(`This is the list of our students\n${students}`);
-  //   } catch (error) {
-  //     response_obj.end(`Error: ${error.message}`);
-  //   }
-  // }
+  const databasePath = process.argv[2];
+  if (!databasePath) {
+    response_obj.status(400).send(
+      'Database name must be provided as an argument'
+    );
+    return;
+  }
+  try {
+    // use await bc countstudents is
+    // an async operation which returns a promise
+    const students = await countStudents(databasePath);
+    response_obj.send(`This is the list of our students\n${students}`);
+  } catch (error) {
+    response_obj.status(500).send(`Error: ${error.message}`);
+  }
 });
 app.listen(port, function () {
   console.log(`App listening on port ${port}`);

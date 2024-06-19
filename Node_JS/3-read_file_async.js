64d968fc5340d88d8a9ca59fdf9aac9801bfb6d0
mrktptn <marktipton95@gmail.com>
2024-06-18 22:33:18 -0500

    used response object to display plain text in body of endpoint page

--- Node_JS/3-read_file_async.js
+++ Node_JS/3-read_file_async.js
@@ -1,8 +1,40 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
-function countStudents(path) {
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const csv_lines = data.split('\n').filter(line => line.trim() !== '');
+    // take off first line to isolate student data
+    const students = csv_lines.slice(1);
 
-  fs.readFile(path)
+    const students_by_field = {
+      CS: [],
+      SWE: [],
+    };
+
+    students.forEach((student) => {
+      // split line by commas to isolate types of info
+      const row = student.split(',');
+      const firstName = row[0].trim();
+      const field = row[3].trim();
+      if (field === 'CS') {
+        students_by_field.CS.push(firstName);
+      } else if (field === 'SWE') {
+        students_by_field.SWE.push(firstName);
+      }
+    });
+
+    console.log(`Number of students: ${students.length}`);
+
+    for (const field in students_by_field) {
+      const len = students_by_field[field].length;
+      console.log(`Number of students in ${field}: ${len}. ` +
+        `List: ${students_by_field[field].join(', ')}`
+      );
+    };
+  } catch (error) {
+    console.error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;

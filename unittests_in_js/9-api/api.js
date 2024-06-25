036df08d5e72022d25e30c9e9c9b65d7fbf43a04
mrktptn <marktipton95@gmail.com>
2024-06-24 21:54:19 -0500

    tests passing locally

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -8,7 +8,13 @@ app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id')
+app.get('/cart/:id', (request, response) => {
+  const { id } = request.params;
+  if (isNaN(id)) {
+    return response.status(404).send('Id must be a number');
+  }
+  return response.send(`Payment methods for cart ${id}`);
+});
 
 app.listen(port, () => {
   console.log(`API available on ${host} port ${port}`);
--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -32,3 +32,35 @@ describe('Index Page', function() {
       });
   });
 });
+
+// cart page tests
+describe('Cart Page', function() {
+  it('should return status code 200 when id is number', function(done) {
+    chai.request('http://localhost:7865')
+      .get('/cart/33')
+      .end(function(error, response) {
+        expect(response).to.have.status(200);
+        expect(response.text).to.equal('Payment methods for cart 33');
+        done();
+      });
+  });
+
+  it('should return status code 404 when id is NaN', function(done) {
+    chai.request('http://localhost:7865')
+      .get('/cart/umhello?')
+      .end(function(error, response) {
+        expect(response).to.have.status(404);
+        expect(response.text).to.equal('Id must be a number');
+        done();
+      });
+  });
+
+  it('should have correct return', function(done) {
+    chai.request('http://localhost:7865')
+      .get('/')
+      .end(function(error, response) {
+        expect(response.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  });
+});

ef31df15d2af13b76c9deb6cd21ba419bd56c650
mrktptn <marktipton95@gmail.com>
2024-06-26 14:20:39 -0500

    added async and await to get sequencing of output correct

--- queuing_system_in_js/2-redis_op_async.js
+++ queuing_system_in_js/2-redis_op_async.js
@@ -1,9 +1,10 @@
-import { resolve } from 'path';
 import redis from 'redis';
 import { promisify } from 'util';
 
 const client = redis.createClient();
 const promisifiedGet = promisify(client.get).bind(client);
+const promisifiedSet = promisify(client.set).bind(client);
+
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
@@ -13,13 +14,13 @@ client.on('error', (error) => {
   console.error(`Redis client not connected to the server: ${error}`);
 });
 
-function setNewSchool(schoolName, value) {
-  return new Promise((resolve, reject) => {
-    client.set(schoolName, value, (err, reply) => {
-      resolve(reply);
-      console.log(`Reply: ${reply}`);
-    });
-  });
+async function setNewSchool(schoolName, value) {
+  try {
+    const reply = await promisifiedSet(schoolName, value);
+    console.log(`Reply: ${reply}`);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function displaySchoolValue(schoolName) {
@@ -31,6 +32,8 @@ async function displaySchoolValue(schoolName) {
   }
 }
 
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+(async () => {
+  await displaySchoolValue('Holberton');
+  await setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();

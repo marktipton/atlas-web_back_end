c865235c8528d86c134ff24fbfae41e48395b400
mrktptn <marktipton95@gmail.com>
2024-06-27 10:45:30 -0500

    added subscriber. js

--- queuing_system_in_js/5-subscriber.js
+++ queuing_system_in_js/5-subscriber.js
@@ -9,3 +9,13 @@ client.on('connect', () => {
 client.on('error', (error) => {
   console.error(`Redis client not connected to the server: ${error}`);
 });
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(`Received message from ${channel}: ${message}`);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
